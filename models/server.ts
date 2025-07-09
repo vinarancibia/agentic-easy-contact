941fab6ef608117e6590b70d6734506df2c5d27e
Rodrigo Velasco <rorito125@gmail.com>
2025-07-09 15:02:14 -0400

    Se creo el cloudbuild para el despliegue continuo y se modifico el package.json para agregar el comando start

--- models/server.ts
+++ models/server.ts
@@ -29,7 +29,7 @@ class Server {
 
     listen(){
         this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en el puerto', this.port);
+            console.log('🤩 Servidor corriendo en el puertos: ', this.port);
         })
     }
 }
