c057441612d5d8983ee9fe40c9fc3601e05363b1
Rodrigo Velasco <rorito125@gmail.com>
2025-07-09 17:47:57 -0400

    Console.log para ver como se resive el body desde easyContact

--- controllers/agent.controller.ts
+++ controllers/agent.controller.ts
@@ -4,17 +4,21 @@ import agentMaria from "../agents/maria";
 
 export const chatAgent = async (req: Request, res: Response) => {
     const { message } = req.body;
+    console.log("Aqui esta el body desde easycontact",req.body);
+
+
     if (!message) {
         res.json({ msg: 'Por favor, envia un mensaje con la sintaxis correcta.' });
         return;
     }
-
     const result = await agentMaria.invoke(
         { messages: [{ role: "user", content: message }] },
         { configurable: { thread_id: "user1" } }
     );
 
     const msg = result.messages[result.messages.length - 1].content;
+
+    //* {content}
 
     res.json({
         msg
