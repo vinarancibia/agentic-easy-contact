bc72cf325e19f926bf6911decc8fe3a33127f0a0
Rodrigo Velasco <rorito125@gmail.com>
2025-07-10 11:26:03 -0400

    Verificando el Data

--- controllers/agent.controller.ts
+++ controllers/agent.controller.ts
@@ -3,27 +3,27 @@ import agentMaria from "../agents/maria";
 
 
 export const chatAgent = async (req: Request, res: Response) => {
-    const { message, messages,  conversation, message_type, content, attachments} = req.body;
+    const { message, messages,  data} = req.body;
 
-    console.log("length: ",messages.length);
-    console.log("messages: ",messages[messages.length - 1].content);
-    console.log("message sin S: ",message);
+    console.log("------- DATA -------");
+    console.log(data);
+    console.log("------- DATA -------");
 
 
-    if (!message) {
-        res.json({ msg: 'Por favor, envia un mensaje con la sintaxis correcta.' });
-        return;
-    }
-    const result = await agentMaria.invoke(
-        { messages: [{ role: "user", content: message }] },
-        { configurable: { thread_id: "user1" } }
-    );
+    // if (!message) {
+    //     res.json({ msg: 'Por favor, envia un mensaje con la sintaxis correcta.' });
+    //     return;
+    // }
+    // const result = await agentMaria.invoke(
+    //     { messages: [{ role: "user", content: message }] },
+    //     { configurable: { thread_id: "user1" } }
+    // );
 
-    const msg = result.messages[result.messages.length - 1].content;
+    // const msg = result.messages[result.messages.length - 1].content;
 
     //* {content}
 
     res.json({
-        msg
+        msg:"Msg Generico"
     })
 }
